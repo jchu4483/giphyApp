b1a6ae26ecbb0794d306d8d358176b5e4cbbfcd9
jchu4483 <jchu4483@gmail.com>
2018-04-08 17:13:22 -0400

    reinit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM loaded");
+  var searchButton = document.getElementById('searchButton')
+  var removeButton = document.getElementById('removeButton')
+  var giphyContainer = document.getElementsByClassName('giphyContainer')[0];
+
+  function parseSearchSendRequest() {
+    var giphySearch = document.getElementById('giphySearch').value
+    var giphyKey = ""
+    var giphyxhr = "http://api.giphy.com/v1/gifs/search?q=" + giphySearch + "&api_key=" + giphyKey + "&limit=1";
+
+    var newRequest = new XMLHttpRequest();
+    newRequest.onreadystatechange = function () {
+      if (newRequest.readyState === 4 && newRequest.status === 200) {
+        var returnedGiphy = JSON.parse(newRequest.responseText)
+        console.log(returnedGiphy.data[0].images)
+
+        if (returnedGiphy) {
+          var newImg = document.createElement("IMG");
+          newImg.src = returnedGiphy.data[0].images.downsized_medium.url;
+          newImg.height = returnedGiphy.data[0].images.downsized_medium.height;
+          newImg.width = returnedGiphy.data[0].images.downsized_medium.width;
+          
+          giphyContainer.appendChild(newImg)
+        }
+      }
+    }
+
+    newRequest.open("GET", giphyxhr)
+    newRequest.send();
+  }
+
+  function removeAllGiphys() {
+    while(giphyContainer.hasChildNodes()) {
+      giphyContainer.removeChild(giphyContainer.lastChild)
+    }
+  }
+
+  searchButton.addEventListener('click', parseSearchSendRequest)
+  removeButton.addEventListener('click', removeAllGiphys)
+  
+  
+
+})
